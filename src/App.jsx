4076aeaa247c383a40ef75165becf3e372f97229
shivamji642002 <shivamji642002@gmail.com>
2024-10-15 17:27:40 +0530

    change navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Reports from './pages/Reports/Reports';
 import Settings from './pages/Settings/Settings';
-import Header from './component/Header/Header';
-import Hero from './component/Hero/Hero';
-import Dashboard from './component/Dashboard/Dashboard';
-import LineChart from './component/Charts/LineChart/LineChart';
+
 function App() {
   return (
     <Router>
